622a7608aa4e450040fe2685700417f00f903c9d
pqhuy1987 <pqhuy1987@gmail.com>
2019-01-07 17:21:58 +0700

    Subline Text Project

--- wp-content/themes/Divi/header.php
+++ wp-content/themes/Divi/header.php
@@ -268,8 +268,8 @@
 									<li <?php if ( is_home() ) echo( 'class="current_page_item"' ); ?>><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php esc_html_e( 'Home', 'Divi' ); ?></a></li>
 								<?php }; ?>
 
-								<?php show_page_menu( $menuClass, false, false ); ?>
-								<?php show_categories_menu( $menuClass, false ); ?>
+								<?php //show_page_menu( $menuClass, false, false ); ?>
+								<?php //show_categories_menu( $menuClass, false ); ?>
 							</ul>
 						<?php
 							else :
